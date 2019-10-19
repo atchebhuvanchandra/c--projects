1a8ac850ee3b217d42f4a4c61ba7a14f5697fe8a
atchebhuvanchandra <54194732+atchebhuvanchandra@users.noreply.github.com>
2019-10-19 19:06:07 -0400

    Create ldshr_svc_proc.c

---
+++ OS load sharing in c/ldshr_svc_proc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <rpc/rpc.h>
+#include "ldshr.h"
+extern double reduction(int N,int M,int S)
+
+double getload_1_svc()
+{
+getloadavg(double *res);
+return res;
+}
+double getmax_1_svc(abc *a){
+double y = reduction(abc.N,abc.M,abc.S);
+return &y;
+}
